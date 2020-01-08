87c4be2090086a10c5c9b01717588361d9389530
Gina Lucy <53538316+grlucy@users.noreply.github.com>
2020-01-07 19:19:42 -0500

    updated solution to activity 9-4

--- URActivity9-4-Parameter-Check/js/index.js
+++ URActivity9-4-Parameter-Check/js/index.js
@@ -1,7 +1,8 @@
-if (process.argv[2] === process.argv[3]) {
-  console.log(true);
-  return true;
-} else {
-  console.log(false);
-  return false;
-}
+// if (process.argv[2] === process.argv[3]) {
+//   console.log(true);
+//   return true;
+// } else {
+//   console.log(false);
+//   return false;
+// }
+console.log(process.argv[2] === process.argv[3]);
