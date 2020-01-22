7eaa31bb80a621ab9fb326843db7c5819b41ed18
Gina Lucy <53538316+grlucy@users.noreply.github.com>
2020-01-21 19:46:36 -0500

    solved activity 11-2

---
+++ URActivity11-2-Two-Servers/two-servers.js
@@ -0,0 +1,30 @@
+const http = require("http");
+
+const port1 = 7000;
+const port2 = 7500;
+
+const inspiringQuotes = [
+  "The quickest way to double your money is to fold it over and put it back in your pocket.",
+  "The real opportunity for success lies within the person and not in the job.",
+  "Whoever loves much, performs much, and can accomplish much, and what is done in love is done well.",
+  "Life is 10% what happens to us and 90% how we react to it.",
+  "Be not afraid of life. Believe that life is worth living, and your belief will help create the fact."
+];
+
+function handleClientReq(req, res) {
+  //   let index = Math.floor(Math.random * inspiringQuotes.length);
+  //   let quote = inspiringQuotes[index];
+  res.end(
+    "The quickest way to double your money is to fold it over and put it back in your pocket."
+  );
+}
+
+const server1 = http.createServer(handleClientReq);
+server1.listen(port1, function() {
+  console.log("server1 is listening on: http://localhost:" + port1);
+});
+
+const server2 = http.createServer(handleClientReq);
+server2.listen(port2, function() {
+  console.log("server2 is listening on: http://localhost:" + port2);
+});
