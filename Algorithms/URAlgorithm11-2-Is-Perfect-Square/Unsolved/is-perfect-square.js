be7ce0eade1ff429616deb6620c15a11eb11f7b0
unknown <53538316+grlucy@users.noreply.github.com>
2020-01-28 12:10:44 -0500

    solved algorithm 11-2

---
+++ Algorithms/URAlgorithm11-2-Is-Perfect-Square/Unsolved/is-perfect-square.js
@@ -0,0 +1,11 @@
+// Write code to create a function that accepts a number and returns true if the number is a perfect square, otherwise it returns false.
+// You may not use the built-in Math.sqrt method
+
+var isPerfectSquare = function(num) {
+  for (let i = 0; i <= num; i++) {
+    if (i * i === num) {
+      return true;
+    }
+  }
+  return false;
+};
