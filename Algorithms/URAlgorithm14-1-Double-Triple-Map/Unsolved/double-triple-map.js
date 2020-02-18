8ad0ad56524f6ce8896297772e9ed923bba2c1e2
unknown <53538316+grlucy@users.noreply.github.com>
2020-02-18 14:09:29 -0500

    solved algorithm 14-1

---
+++ Algorithms/URAlgorithm14-1-Double-Triple-Map/Unsolved/double-triple-map.js
@@ -0,0 +1,17 @@
+// Write code to create a function that accepts an array of numbers and returns a new array that corresponds to the original array
+// If a element in the original array is even, the element at the same index in the new array should be double the original element
+// If an element in the original array is odd, the element at the same index of the new array should be triple the original element
+
+var doubleTripleMap = function(arr) {
+  let newArr = [];
+  for (const index of arr) {
+    if (index % 2 === 0) {
+      let newIndex = index * 2;
+      newArr.push(newIndex);
+    } else {
+      let newIndex = index * 3;
+      newArr.push(newIndex);
+    }
+  }
+  return newArr;
+};
